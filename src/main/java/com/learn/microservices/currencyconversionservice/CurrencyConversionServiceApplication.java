263ff73fdeb85bbdf56d1734632c41700edc19e9
Nagarajan <nagaraj.learn@gmail.com>
2019-01-13 13:42:55 +0530

    first commit

---
+++ src/main/java/com/learn/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -0,0 +1,18 @@
+package com.learn.microservices.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients("com.learn.microservice.currencyconversionservice.feignproxy")
+@EnableDiscoveryClient
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+
+}
+
